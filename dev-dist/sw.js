f0ccb897edba2fbdb9c43de5935c9118b916911c
NanoNite <fcknhandsome@gmail.com>
2026-08-26 14:27:32 +0700

    sync code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-caf3a6a6'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.eotmot1189g"
+    "revision": "0.kmi23l1imh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
